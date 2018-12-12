a2fb6cfe0672b9fc4a0f5cee085ac3b1c2121997
Natalia Stafeeva <n.stafeeva@gmail.com>
2018-12-12 16:43:21 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import instaLogo from './images/icons/insta_logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+        <header>
+          <h1>Emily</h1>
+          <p>This website is under construction. Check back soon!</p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            href="https://www.instagram.com/emily_the_afrorussian"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            Learn React
+            >
+            <img
+              className="insta-icon"
+              alt=""
+              src={instaLogo} />
           </a>
         </header>
       </div>
